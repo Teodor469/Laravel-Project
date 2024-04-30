35432496ac6b96c2abe95811e55e63c5fe21b89a
Teodor469 <lifetroll6@gmail.com>
2024-04-30 10:34:30 +0300

    .

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -9,10 +9,15 @@ class IdeaController extends Controller
 {
     public function store()
     {
+
+        request()->validate([
+            'idea' => 'required|min:3|max:240'
+        ]);
+
         $idea = idea::create([
             'content' => request()->get('idea', ''),
         ]);
 
-        return redirect()->route('dashboard');
+        return redirect()->route('dashboard')->with('success', 'Idea was created successfully!');
     }
 }
