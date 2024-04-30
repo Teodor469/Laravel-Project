1a82bd570a11a7d7b8908a5442a9f35404b2ce93
Teodor469 <lifetroll6@gmail.com>
2024-04-30 14:43:52 +0300

    .

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,10 +9,19 @@ class DashboardController extends Controller
 {
     public function index()
     {
+
+        $ideas = idea::orderBy('created_at', 'DESC');
+        // check if there is a search
+        // if there is, check the search value
+
+        if(request()->has('search')) {
+            $ideas = $ideas->where('content', 'like' , '%' . request()->get('search', '') . '%');
+        }
+
         return view(
             'dashboard',
             [
-                'ideas' => idea::orderBy('created_at', 'DESC')->paginate(5)
+                'ideas' => $ideas->paginate(5)
             ]
         );
     }
