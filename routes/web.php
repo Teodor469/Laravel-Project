78c03f1350f504e93f59138d3d18f762a72bff83
Teodor469 <lifetroll6@gmail.com>
2024-05-20 11:41:46 +0300

    .

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\CommentController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\IdeaController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
@@ -23,17 +24,13 @@ use function Pest\Laravel\withoutMiddleware;
 
 Route::get('/', [DashboardController::class, 'index'])->name("dashboard");
 
-Route::group(['prefix' => 'idea/', 'as' => 'idea.',], function () {
-
-    Route::group(['middleware' => ['auth']], function () {
-
-        Route::post('/{idea}/comments', [CommentController::class, 'store'])->name('comments.store');
-    });
-});
-
 Route::resource('idea', IdeaController::class)->except('index', 'create', 'show')->middleware('auth');
+
 Route::resource('idea', IdeaController::class)->only('show');
-// Route::resource('idea.comments', IdeaController::class)->only('store')->middleware('auth');
+
+Route::resource('idea.comments', CommentController::class)->only('store')->middleware('auth');
+
+Route::resource('users', UserController::class)->only('show', 'edit', 'update')->middleware('auth');
 
 // Route::get('/terms', [ProfileController::class , 'index']);
 
