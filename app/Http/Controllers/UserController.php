2bdee9def00d649288d7e9d6bbd38c9b89856467
Teodor469 <lifetroll6@gmail.com>
2024-05-21 11:34:16 +0300

    .

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -25,7 +25,7 @@ class UserController extends Controller
     {
         $ideas = $user->ideas()->paginate(5);
         $editing = true;
-        return view('users.show', compact('user', 'editing', 'ideas'));
+        return view('users.edit', compact('user', 'editing', 'ideas'));
     }
 
     /**
@@ -33,7 +33,22 @@ class UserController extends Controller
      */
     public function update(User $user)
     {
-        //
+        $validated = request()->validate(
+            [
+                'name' => 'required|min:3|max:40',
+                'bio' => 'nullable|min:1|max:255',
+                'image' => 'image',
+            ],
+        );
+
+        if (request()->has('image')) {
+            $imagePath = request()->file('image')->store('profile', 'public');
+            $validated['image'] = $imagePath;
+        }
+
+        $user->update($validated);
+
+        return redirect()->route('profile');
     }
 
     public function profile()
